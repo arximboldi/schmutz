2c373f5499a6e3e2c0aa27cb513e436fc671f9c5
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2017-07-25 11:48:50 +0200

    Install all required dependencies via Nix

--- shell.nix
+++ shell.nix
@@ -1,5 +1,28 @@
 with import <nixpkgs> {};
 
+let callable_traits_commit = "684dfbd7dfbdd0438ef3670be10002ca33a71715"; in
+
+let callable_traits = stdenv.mkDerivation rec {
+  name = "callable_traits-${version}";
+  version = callable_traits_commit;
+
+  src = fetchFromGitHub {
+    owner = "badair";
+    repo = "callable_traits";
+    rev = callable_traits_commit;
+    sha256 = "1qnprz2lznm6b90xsj7jl2bk5cbvks4mzv5l8ks7cqpwyi128w0n";
+  };
+
+  nativeBuildInputs = [ cmake ];
+  enableParallelBuilding = true;
+
+  meta = with stdenv.lib; {
+    homepage    = "https://github.com/badair/callable_traits";
+    description = "C++11 header-only library for the inspection, synthesis, and decomposition of callable types";
+    license     = licenses.boost;
+  };
+}; in
+
 stdenv.mkDerivation rec {
   name = "schmutz-dev-env";
   env = buildEnv { name = name; paths = buildInputs; };
@@ -11,5 +34,6 @@ stdenv.mkDerivation rec {
     cmake
     doxygen
     python27Packages.pip
+    callable_traits
   ];
 }
