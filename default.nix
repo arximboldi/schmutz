cbb9523ba5f933bc2f2b84927f994012525baa10
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2017-07-25 13:02:12 +0200

    Extract Nix derivation for Schmutz itself

--- default.nix
+++ default.nix
@@ -1,39 +1,65 @@
+/*
+ * nix specifications
+ */
+
 with import <nixpkgs> {};
 
 let callable_traits_commit = "684dfbd7dfbdd0438ef3670be10002ca33a71715"; in
 
-let callable_traits = stdenv.mkDerivation rec {
+rec {
+
+callable_traits = stdenv.mkDerivation rec {
   name = "callable_traits-${version}";
   version = callable_traits_commit;
-
   src = fetchFromGitHub {
     owner = "badair";
     repo = "callable_traits";
     rev = callable_traits_commit;
     sha256 = "1qnprz2lznm6b90xsj7jl2bk5cbvks4mzv5l8ks7cqpwyi128w0n";
   };
-
   nativeBuildInputs = [ cmake ];
   enableParallelBuilding = true;
-
   meta = with stdenv.lib; {
     homepage    = "https://github.com/badair/callable_traits";
     description = "C++11 header-only library for the inspection, synthesis, and decomposition of callable types";
     license     = licenses.boost;
   };
-}; in
+};
 
-stdenv.mkDerivation rec {
-  name = "schmutz-dev-env";
-  env = buildEnv { name = name; paths = buildInputs; };
-  buildInputs = [
-    gcc6
-    clang_38
-    guile
-    pkgconfig
+schmutz = stdenv.mkDerivation rec {
+  name = "schmutz-git";
+  version = "git";
+  src = builtins.filterSource (path: type:
+            baseNameOf path != ".git" &&
+            baseNameOf path != "build")
+          ./.;
+  nativeBuildInputs = [
     cmake
-    doxygen
-    python27Packages.pip
+    pkgconfig
+  ];
+  propagatedBuildInputs = [
+    guile
     callable_traits
   ];
+  enableParallelBuilding = true;
+  meta = with stdenv.lib; {
+    homepage    = "https://github.com/arximboldi/schmutz";
+    description = "Easy bindings between C++ and Scheme";
+    license     = licenses.boost;
+  };
+};
+
+schmutz_env = stdenv.mkDerivation rec {
+  name = "schmutz-env";
+  env = buildEnv { name = name; paths = buildInputs; };
+  buildInputs = [
+    schmutz
+    cmake
+    gcc6
+    clang_38
+    doxygen
+    python27Packages.pip
+  ];
+};
+
 }
